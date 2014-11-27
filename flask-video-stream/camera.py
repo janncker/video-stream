c1cb858ad874d826ae13084593b5f9c0e353179f
Janncker <janncker@gmail.com>
2014-11-27 16:29:15 +0000

    Enable face dectection

--- flask-video-stream/camera.py
+++ flask-video-stream/camera.py
@@ -1,18 +1,32 @@
 import time
 import cv2
 
+faceCascade = cv2.CascadeClassifier("haarcascade.xml")
+
 class Camera(object):
     """An emulated camera implementation that streams a repeated sequence of
     files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
 
     def __init__(self):
 	self.cam = cv2.VideoCapture(0) 	
-	self.cam.set(3, 800)
-	self.cam.set(4, 600)
+	self.cam.set(3, 320)
+	self.cam.set(4, 240)
 	time.sleep(1)
 
     def get_frame(self):
 	ret, img = self.cam.read()
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	faces = faceCascade.detectMultiScale(
+		gray,
+		scaleFactor=1.1,
+		minNeighbors=5,
+		minSize=(30, 30),
+		flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+	)
+	# Draw a rectangle around the faces
+	for (x, y, w, h) in faces:
+		cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
 	ret2, jpeg = cv2.imencode('.jpg', img)
 	return jpeg.tostring() 
 
